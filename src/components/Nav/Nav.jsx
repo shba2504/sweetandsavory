c45f0475de5374e0367c2d9e3499d76a2b1837af
Sheena Barnes <shba2504@gmail.com>
2024-01-11 11:27:52 -0700

    styled navbar desktop and mobile

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,48 @@
+import "../Nav/Nav.css";
+import Images from "../../images/images";
+
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
+
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
+
 const Nav = () => {
   return (
     <>
       <nav class="navbar">
-        <div>Logo</div>
+        <div class="logo">
+          <img src={Images.Logo} alt="half-lemon and restaurant name logo" />
+        </div>
+        <a href="#" class="toggle-button">
+          <span class="bar"></span>
+          <span class="bar"></span>
+          <span class="bar"></span>
+        </a>
         <div class="navbar-links">
           <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Menu</li>
-            <li>Reservations</li>
-            <li>Order Online</li>
-            <li>Log In</li>
-            <li>Register</li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Menu</a>
+            </li>
+            <li>
+              <a href="#">Reservations</a>
+            </li>
+            <li>
+              <a href="#">Order Online</a>
+            </li>
+            <li>
+              <a href="#">Log In</a>
+            </li>
+            <li>
+              <a href="#">Register</a>
+            </li>
           </ul>
         </div>
       </nav>
---
+++ src/images/images.jsx
@@ -0,0 +1,5 @@
+const Images = {
+  Logo: new URL("./Logo.svg", import.meta.url).href,
+};
+
+export default Images;
