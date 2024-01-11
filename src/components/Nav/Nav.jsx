e2f7c43e10242ea661e8109004474a47b09c117d
Sheena Barnes <shba2504@gmail.com>
2024-01-10 22:14:55 -0700

    added nav setup

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,22 @@
 const Nav = () => {
-  return <h1>Nav</h1>;
+  return (
+    <>
+      <nav class="navbar">
+        <div>Logo</div>
+        <div class="navbar-links">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Log In</li>
+            <li>Register</li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default Nav;
