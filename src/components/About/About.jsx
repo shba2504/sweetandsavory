510c80912e5f61ca673feedccde2adafce081862
Sheena Barnes <shba2504@gmail.com>
2024-01-11 22:10:37 -0700

    initial code for About and Reservations pages

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,12 +1,64 @@
 import Nav from "./Nav/Nav";
+import Images from "../../images/images";
 
 const About = () => {
   return (
     <>
       <Nav />
-      <div class="preview">
-        <h1>About Us</h1>
-      </div>
+      <article>
+        <section>
+          <div class="preview">
+            <h1>About Us</h1>
+            <img src={Images.globe} alt="a half-globe" />
+          </div>
+          <section>
+            <div class="about">
+              <img
+                src={Images.travelfood}
+                alt="plate of pancakes with person eating"
+              />
+              <p>
+                From chicken and waffles in the American South to Danish
+                pancakes to North African shakshouka and French toast in its
+                Roman origin...
+              </p>
+              <p>
+                Mark and Liza have experienced a variety of different
+                cuisines...
+              </p>
+              <img
+                src={Images.shakshouka}
+                alt="skillet with tomato sauce and eggs"
+              />
+              <img
+                src={Images.DanishPancakes}
+                alt="Danish pancakes that look like mini-donuts and sauce"
+              />
+              <img
+                src={Images.travelfood2}
+                alt="person slicing into pancakes"
+              />
+              <img
+                src={Images.gathering}
+                alt="people toasting over plates of food"
+              />
+              <p>
+                Inspired by simple ingredients executed perfectly, Mark and Liza
+                opened Sweet and Savory in 2008 to bring people together in the
+                best way---over great food.
+              </p>
+            </div>
+          </section>
+          <section>
+            <div class="bottom">
+              <img
+                src={Images.germanbreakfast}
+                alt="table spread of traditional German breakfast with eggs and lots of bread"
+              />
+            </div>
+          </section>
+        </section>
+      </article>
     </>
   );
 };
--- src/components/Reservations/Reservations.jsx
+++ src/components/Reservations/Reservations.jsx
@@ -1,10 +1,52 @@
 import Nav from "./Nav/Nav";
+import Images from "../../images/images";
 
 const Reservations = () => {
   return (
     <>
       <Nav />
-      <h1>Reservations</h1>
+      <article>
+        <section>
+          <div class="preview">
+            <h1>Reservations</h1>
+            <img
+              src={Images.exterior}
+              alt="wooden table with plate and glass settings"
+            />
+          </div>
+        </section>
+        <section>
+          <div>
+            <img
+              src={Images.granola}
+              alt="bowl of granola with pieces of granola and strawberries scattered on table"
+            />
+            <form>
+              <fieldset>
+                <label>
+                  Party Size
+                  <input />
+                </label>
+                <label>
+                  Date
+                  <input />
+                </label>
+                <label>
+                  Time
+                  <input />
+                </label>
+              </fieldset>
+            </form>
+          </div>
+        </section>
+        <hr />
+        <div class="bottom">
+          <img
+            src={Images.greenrestaurant}
+            alt="restaurant interior with lots of plants"
+          />
+        </div>
+      </article>
     </>
   );
 };
