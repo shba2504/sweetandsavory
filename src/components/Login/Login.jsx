a2728059f664820347680d35bb454bbfafda6ca2
Sheena Barnes <shba2504@gmail.com>
2024-01-17 22:45:53 -0700

    Login page updates

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,70 +1,43 @@
 import React from "react";
 import Nav from "../Nav/Nav";
 import "../Login/Login.css";
-import { Formik } from "formik";
-import * as Yup from "yup";
 
-const Login = (props) => {
+const Login = () => {
   return (
     <>
       <Nav />
       <article>
         <section>
           <div className="preview">
-            <h1>Welcome!</h1>
-            <p>
-              Log in below or <a href="/">create an account</a>
-            </p>
+            <h1>Welcome Back!</h1>
           </div>
         </section>
         <section>
-          <Formik
-            initialValues={{ email: "", password: "" }}
-            validationSchema={Yup.object({
-              email: Yup.string()
-                .email("Invalid email address")
-                .required("Required"),
-              password: Yup.string().required("No password provided."),
-            })}
-            onSubmit={(values, { setSubmitting }) => {
-              setTimeout(() => {
-                alert(JSON.stringify(values, null, 2));
-                setSubmitting(false);
-              }, 400);
-            }}
-          >
-            {(formik) => (
-              <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="email">Email Address</label>
-                <input
-                  id="email"
-                  type="email"
-                  {...formik.getFieldProps("email")}
-                />
-                {formik.touched.email && formik.errors.email ? (
-                  <div>{formik.errors.email}</div>
-                ) : null}
+          <div className="full-screen-container">
+            <div className="login-container">
+              <form>
+                <div className="input-group">
+                  <p>
+                    Log in below or <a href="/">create an account</a>
+                  </p>
+                  <label htmlFor="email">Email Address</label>
+                  <input id="email" type="email" />
 
-                <label htmlFor="password">Password</label>
-                <input
-                  id="password"
-                  type="password"
-                  {...formik.getFieldProps("password")}
-                />
-                {formik.touched.password && formik.errors.password ? (
-                  <div>{formik.errors.password}</div>
-                ) : null}
+                  <label htmlFor="password">Password</label>
+                  <input id="password" type="password" />
+
+                  <div>
+                    <button id="login-button" type="submit" aria-label="login-submit">
+                      Sign In
+                    </button>
+                    <p>Forgot Password?</p>
+                  </div>
+                </div>
               </form>
-            )}
-          </Formik>
+            </div>
+          </div>
         </section>
         <br />
-        <section>
-          <div>
-            <button type="submit">Sign In</button>
-            <p>Forgot Password?</p>
-          </div>
-        </section>
       </article>
     </>
   );
