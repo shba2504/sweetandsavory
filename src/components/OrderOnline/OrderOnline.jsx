8d2259e7af2b437a2e1dc0e09220c8c3e78e3c8f
Sheena Marshalla Barnes <shba2504@gmail.com>
2024-02-26 11:46:51 -0700

    return to original

--- src/components/OrderOnline/OrderOnline.jsx
+++ src/components/OrderOnline/OrderOnline.jsx
@@ -9,7 +9,7 @@ const recipes = [
     name: "Blueberry Banana French Toast",
     description: "Served on Brioche bread w/ a side of goat cheese.",
     price: "$14",
-    photoName: "assets/blueberryfrenchtoast.jpg",
+    photoName: "src/images/blueberryfrenchtoast.jpg",
     button: "Add to Cart",
   },
   {
