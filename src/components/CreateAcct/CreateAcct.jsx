6fe6a9c1f521adb98d7b2b27d034962b60f9b655
Sheena Marshalla Barnes <shba2504@gmail.com>
2024-03-02 19:09:18 -0700

    mobile adjustments

--- src/components/CreateAcct/CreateAcct.jsx
+++ src/components/CreateAcct/CreateAcct.jsx
@@ -11,36 +11,38 @@ const CreateAcct = () => {
   return (
     <>
       <Nav />
-      <div className="preview">
-        <h1>Create an Account</h1>
-        <p>
-          Earn rewards when you create an account or{" "}
-          <Link to="/login"> Log In</Link>
-        </p>
-      </div>
-
-      <div className="input-container">
-        <div className="form-container">
-          <form className="input-group">
-            <FirstName />
-            <LastName />
-            <EmailAddress />
-            <Password />
-            <br />
-            <div>
-              <button type="submit">Sign In</button>
-              <input
-                type="checkbox"
-                name="acceptedNewsletter"
-                aria-label="click checkbox to join our newsletter"
-              />
-              <p>Join our monthly newsletter to view our upcoming specials!</p>
-            </div>
-          </form>
+      <div className="account-page">
+        <div className="preview">
+          <h1 className="account">Create an Account</h1>
+          <p>
+            Earn rewards when you create an account or{" "}
+            <Link to="/login"> Log In</Link>
+          </p>
         </div>
-      </div>
 
-      <br />
+        <div className="input-container">
+          <div className="form-container">
+            <form className="input-group">
+              <FirstName />
+              <LastName />
+              <EmailAddress />
+              <Password />
+              <br />
+              <div>
+                <button type="submit">Sign In</button>
+                <input
+                  type="checkbox"
+                  name="acceptedNewsletter"
+                  aria-label="click checkbox to join our newsletter"
+                />
+                <p>Join our monthly newsletter to view our upcoming specials!</p>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <br />
+        </div>
     </>
   );
 };
